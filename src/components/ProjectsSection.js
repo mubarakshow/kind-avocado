3ab99e45772d43081bf1c8960c64454decf6fd2f
Mubarak Show <mubarak.show@gmail.com>
2021-03-12 09:05:12 +0100

    refactored ProjectsSection from class to functional component

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -11,39 +11,40 @@ import Modal from "react-modal";
 
 import { classNames, withPrefix, markdownify } from "../utils";
 
-export default class ProjectSection extends React.Component {
-  constructor() {
-    this.state = {
-      modalIsOpen: false,
-    };
-    this.handleModalClose = this.handleModalClose.bind(this);
-    this.handleModalOpen = this.handleModalOpen.bind(this);
-  }
+const ProjectsSection = () => {
 
-  render() {
-    let section = _.get(this.props, "section", null);
-    console.log('section Data', section)
-    return (
-      <section className={classNames("section")}>
-        {_.get(section, "title", null) && (
-          <div className="container container--md align-center">
-            <h2 className="section__title">{_.get(section, "title", null)}</h2>
-          </div>
-        )}
-        <div className="container container--lg">
-          {_.map(_.get(section, "projects", null), (project, project_idx) => {
-            // add project card. display in a grid
-            console.log('project data', project)
-            return (
-              <div key={project_idx}>
-                {_.map(_.get(project, 'images', null ), (image, image_idx) => (
-                  <img src={withPrefix(_.get(image, 'project_photo', null))} alt={_.get(image, 'project_photo_alt_text', null)} />
-                ))}
-              </div>
-            );
-          })}
+  let section = _.get(this.props, "section", null);
+
+  useEffect(() => {
+    console.log('section', section)
+  }, [])
+
+  return (
+    <section className={classNames("section")}>
+      {_.get(section, "title", null) && (
+        <div className="container container--md align-center">
+          <h2 className="section__title">{_.get(section, "title", null)}</h2>
         </div>
-      </section>
-    );
-  }
-}
+      )}
+      <div className="container container--lg">
+        {_.map(_.get(section, "projects", null), (project, project_idx) => {
+          // add project card. display in a grid
+          console.log("project data", project);
+          return (
+            <div key={project_idx}>
+              {_.map(_.get(project, "images", null), (image, image_idx) => (
+                <img
+                  key={image_idx}
+                  src={withPrefix(_.get(image, "project_photo", null))}
+                  alt={_.get(image, "project_photo_alt_text", null)}
+                />
+              ))}
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default ProjectsSection;
