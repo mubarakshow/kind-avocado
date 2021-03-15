7c2f10602a7683573b9e6ba7ae6eb07eb71aed96
Mubarak Show <mubarak.show@gmail.com>
2021-03-15 17:37:13 +0100

    add update transporter from mailhog to nodemailerTest

--- netlify/functions/sendemail.js
+++ netlify/functions/sendemail.js
@@ -1,16 +1,23 @@
-const { Mailer } = require("../../services/mailer")
+const { Mailer } = require("../../services/mailer");
 
-const trilineNG = new Mailer()
+// const trilineNG = new Mailer();
+const nodemailerTest = new Mailer({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+      user: 'delbert.stanton82@ethereal.email',
+      pass: 'BvhZYW5farSh7UWmGu'
+  }
+});
 
 exports.handler = async (event) => {
+  console.log("netlify functions event object", event.body);
+  const { senderEmail, senderName, subject, message } = JSON.parse(event.body);
 
-  console.log('netlify functions event object', event.body)
-  const { senderEmail, senderName, subject, message } = JSON.parse(event.body)
-
-  trilineNG.sendEmail(
+  nodemailerTest.sendEmail(
     `"${senderName}" <${senderEmail}>`,
-    'mubarak.show@gmail.com',
+    "mubarak.show@gmail.com",
     subject,
     message
-  )
-}
+  );
+};
