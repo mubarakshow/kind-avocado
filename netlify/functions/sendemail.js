426c04e0288c801730700d9a5e87fbd80d5e4e24
Mubarak Show <mubarak.show@gmail.com>
2021-03-22 13:35:47 +0100

    update email template and destination

--- netlify/functions/sendemail.js
+++ netlify/functions/sendemail.js
@@ -1,53 +1,91 @@
 const nodemailer = require("nodemailer");
 
-const nodemailerTestTransport = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
+// const nodemailerTestTransport = nodemailer.createTransport({
+//   host: "smtp.ethereal.email",
+//   port: 587,
+//   service: "",
+//   secure: false,
+//   auth: {
+//     user: "delbert.stanton82@ethereal.email",
+//     pass: "BvhZYW5farSh7UWmGu",
+//   },
+// });
+
+// const mailhogTransporter = nodemailer.createTransport({
+//   host: "localhost",
+//   service: "mailhog",
+//   port: 1025,
+//   secure: false, // true for 465, false for other ports
+//   pool: true,
+//   logger: true,
+//   // auth: {
+//   //   user: 'user' ,
+//   //   pass: 'pass',
+//   // }
+// })
+
+const signsAtTrilinengTransporter = nodemailer.createTransport({
+  host: "mail.linesworth.com",
+  port: 465,
   service: "",
-  secure: false,
+  secure: true,
   auth: {
-    user: "delbert.stanton82@ethereal.email",
-    pass: "BvhZYW5farSh7UWmGu",
+    user: process.env.CONTACT_EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
   },
 });
 
-const mailhogTransporter = nodemailer.createTransport({
-  host: "localhost",
-  service: "mailhog",
-  port: 1025,
-  secure: false, // true for 465, false for other ports
-  pool: true,
-  logger: true,
-  // auth: {
-  //   user: 'user' ,
-  //   pass: 'pass',
-  // }
-})
-
 exports.handler = async function (event, context) {
   const { senderEmail, senderName, subject, message } = JSON.parse(event.body);
-  
+
   const response = await new Promise((resolve, reject) => {
-    mailhogTransporter.sendMail(
+    signsAtTrilinengTransporter.sendMail(
       {
-        from: `"${senderName}" <${senderEmail}>`,
+        from: `"Triline NG" <${process.env.CONTACT_EMAIL}>`,
         to: process.env.CONTACT_EMAIL,
-        subject: subject,
+        subject: `${
+          subject == "Other" ? "Enquiry from " + senderName : subject + " from  " + senderName 
+        }`,
         text: message,
-        html: `<h2>${message}</h2>`,
+        html: `
+          <html>
+            <head>
+              <style>
+              // css goes in here
+                .mailbody {
+                  border: thick double #ffe000;
+                  padding: 1em 2em;
+                }
+              </style>
+            </head>
+            <body>
+              <div class="mailbody">
+                <p><b>Dear Triline NG<b>,</p>
+                <br />
+                <p>You have a message!</p>
+                <p>Sender's Name: <b>${senderName}</b></p>
+                <p>Sender's Email: <b>${senderEmail}</b></p>
+                <p>Subject: <b>${subject}</b></p>
+                <br />
+                <p><b>===== Message =====</b></p>
+                <p>${message}</p>
+              </div>
+            </body> 
+          </html>        
+        `,
       },
       (error, info) => {
         if (error) {
-          return reject("error sending email", error.message);
+          return reject(`error sending email: ${error.message}`, error.message);
         } else {
           resolve(info);
         }
       }
     );
   });
-  
+
   return {
     statusCode: 200,
-    body: JSON.stringify(response)
-  }  
-};
+    body: JSON.stringify(response),
+  };
+  };
